b32250998e2ead922016cc340dd6c06aa2e8ae03
Ayushi saxena <saxenaayu2106@gmail.com>
2024-06-11 08:04:38 +0530

    REST API

---
+++ src/main/java/org/gs/Movie.java
@@ -0,0 +1,21 @@
+package org.gs;
+
+public class Movie {
+    
+    private Long id;
+    private String movieName;
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getMovieName() {
+        return movieName;
+    }
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+
+}
